65b4217ed8801d329fa96f226fdffa79ee03486b
Nicolas Coulon <NICOLAS.X.COULON@GSK.COM>
2021-11-23 15:46:57 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 'use strict'
 
-const clients = require('./lib/clients')
-const brokers = require('./lib/brokers')
+const channelNet = require('./lib/channels/net')
+const channelWs = require('./lib/channels/ws')
+const pluginNet = require('./lib/plugins/net')
+const pluginWs = require('./lib/plugins/ws')
+const codec123 = require('./lib/codecs/123')
+const codecWs = require('./lib/codecs/ws')
+const common = require('./lib/common')
+const logger = require('./lib/logger')
+const constants = require('./lib/constants')
 
 module.exports = {
-  rpc: {
-    broker: brokers.rpc,
-    client: {
-      net: (connectOptions) => {
-        const tcpChannel = clients.channels.net(connectOptions.host, connectOptions.port)
-        return clients.rpcClient(tcpChannel)
-      },
-      ws: (connectOptions) => {
-        const wsChannel = clients.channels.ws(connectOptions.host, connectOptions.port, connectOptions.ssl)
-        return clients.rpcClient(wsChannel)
-      }
-    },
-    server: {
-      net: (connectOptions, modulePath, options) => {
-        const tcpChannel = clients.channels.net(connectOptions.host, connectOptions.port)
-        return clients.rpcServer(tcpChannel, modulePath, options)
-      },
-      ws: (connectOptions, modulePath, options) => {
-        const wsChannel = clients.channels.ws(connectOptions.host, connectOptions.port, connectOptions.ssl)
-        return clients.rpcServer(wsChannel, modulePath, options)
-      }
-    }
-  },
-  pubsub: {
-    broker: brokers.pubsub,
-    client: {
-      net: (options) => {
-        const tcpChannel = clients.channels.net(options.host, options.port)
-        return clients.pubsub(tcpChannel)
-      },
-      ws: (options) => {
-        const wsChannel = clients.channels.ws(options.host, options.port, options.ssl)
-        return clients.pubsub(wsChannel)
-      }
-    }
+  channels: {
+    net: channelNet.create,
+    ws: channelWs.create
   },
   plugins: {
-    broker: brokers.plugins
-  }
+    net: pluginNet.create,
+    ws: pluginWs.create
+  },
+  codecs: {
+    123: codec123,
+    ws: codecWs
+  },
+  common,
+  logger,
+  constants
 }

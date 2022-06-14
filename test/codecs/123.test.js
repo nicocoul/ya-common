d780ec058d3997b80cd2b40d14c9be2dc279c3fe
Nicolas Coulon <NICOLAS.X.COULON@GSK.COM>
2022-06-14 09:12:35 +0200

    strengthen 123 codec

--- test/codecs/123.test.js
+++ test/codecs/123.test.js
@@ -2,10 +2,14 @@ const { pause, newAccumulator } = require('../common')
 const { Readable } = require('stream')
 const {
   newDecoder,
-  newEncoder
+  newEncoder,
+  encode
 } = require('../../lib/codecs/123')
 
 const bytes = [1, 0, 0, 0, 1, 2, 49, 3]
+const bytesInvalidFooter = [1, 0, 0, 0, 1, 2, 49, 4]
+const bytesInvalidHeader = [2, 0, 0, 0, 1, 2, 49, 3]
+const bytesInvalidJSON = [1, 0, 0, 0, 1, 2, 0, 3]
 
 test('it encodes one object', async () => {
   const acc = newAccumulator()
@@ -46,4 +50,83 @@ test('it decodes multiple buffers', async () => {
   await pause(0)
   expect(acc.data()).toHaveLength(1000)
   expect(acc.data()[0]).toStrictEqual(1)
+})
+
+test('it decodes when message is split after the header', async () => {
+  const validBuffer = encode('hello world')
+  const acc = newAccumulator()
+  const decoder = newDecoder()
+  decoder.pipe(acc)
+  decoder.write(validBuffer.slice(0, validBuffer.length - 2))
+  await pause(10)
+  decoder.write(validBuffer.slice(validBuffer.length - 2))
+  await pause(10)
+  expect(acc.data()).toStrictEqual(['hello world'])
+})
+
+test('it decodes when message is split in the header', async () => {
+  const validBuffer = encode('hello world')
+  const acc = newAccumulator()
+  const decoder = newDecoder()
+  decoder.pipe(acc)
+  decoder.write(validBuffer.slice(0, 3))
+  await pause(10)
+  decoder.write(validBuffer.slice(3))
+  await pause(10)
+  expect(acc.data()).toStrictEqual(['hello world'])
+})
+
+test('it decodes when invalid header', async () => {
+  const validBuffer = encode('hello world')
+  const acc = newAccumulator()
+  const decoder = newDecoder()
+  decoder.pipe(acc)
+
+  decoder.write(Buffer.from(bytesInvalidHeader))
+  await pause(10)
+  decoder.write(validBuffer)
+  await pause(10)
+  decoder.write(Buffer.from(bytesInvalidHeader))
+  await pause(10)
+  decoder.write(validBuffer)
+  await pause(10)
+  expect(acc.data()).toStrictEqual(['hello world', 'hello world'])
+})
+
+test('it decodes when invalid footer', async () => {
+  const validBuffer = encode('hello world')
+  const acc = newAccumulator()
+  const decoder = newDecoder()
+  decoder.pipe(acc)
+
+  decoder.write(Buffer.from(bytesInvalidFooter))
+  await pause(10)
+  decoder.write(validBuffer)
+  await pause(10)
+  decoder.write(Buffer.from(bytesInvalidFooter))
+  await pause(10)
+  decoder.write(validBuffer)
+  await pause(10)
+  expect(acc.data()).toStrictEqual(['hello world', 'hello world'])
+})
+
+test('it decodes multiples messages at once', async () => {
+  const validBuffer = Buffer.concat([
+    Buffer.from(bytesInvalidHeader),
+    encode('hello world1'),
+    Buffer.from(bytesInvalidJSON),
+    encode('hello world2'),
+    Buffer.from(bytesInvalidFooter),
+    encode('hello world3'),
+    Buffer.from(bytesInvalidFooter),
+    Buffer.from(bytesInvalidHeader)
+  ])
+  const acc = newAccumulator()
+  const decoder = newDecoder()
+  decoder.pipe(acc)
+  decoder.write(validBuffer)
+  await pause(10)
+  decoder.write(encode('hello world4'))
+  await pause(10)
+  expect(acc.data()).toStrictEqual(['hello world1', 'hello world2', 'hello world3', 'hello world4'])
 })
